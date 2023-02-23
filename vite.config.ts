b2bf2dfc5ae17014b6f070e87ee5c55fb09bae09
Jeremy Magland <jmagland@flatironinstitute.org>
2023-02-23 16:17:56 -0500

    don't disable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,14 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA( // this may have been causing problems. disabling for now
-    //   {
-    //     registerType: 'autoUpdate',
-    //     devOptions: {
-    //       enabled: true
-    //     }
-    //   }
-    // )
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        }
+      }
+    )
   ],
   resolve: {
     alias: {
